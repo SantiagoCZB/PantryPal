b06c31c9240071db4dde2a68635fe582b91934bd
Rafael Reyes <xrafael004@gmail.com>
2024-09-10 09:53:09 -0600

    Nueva vista AgregarIngredientes

---
+++ PantryPal/AgregarIngredientesView.swift
@@ -0,0 +1,18 @@
+//
+//  AgregarIngredientesView.swift
+//  PantryPal
+//
+//  Created by Alumno on 10/09/24.
+//
+
+import SwiftUI
+
+struct AgregarIngredientesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgregarIngredientesView()
+}
