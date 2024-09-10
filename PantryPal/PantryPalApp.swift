edc706fc4f1ec17a0504fb2c6da0286d8ce3c5a3
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-09-10 09:33:13 -0600

    Inicial

---
+++ PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by MacBook Air on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
