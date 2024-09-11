b6ed6f4eeda855f7982cd0fa068c1cc8c71ed830
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-09-11 09:29:36 -0600

    API

--- PantryPal/IngredientesView.swift
+++ PantryPal/IngredientesView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct IngredientesView: View {
     @State private var ingredientes: [String] = []
     @State private var showAgregarIngredientes = false
+    @State private var recetas: [Receta] = [] // Array para guardar las recetas
 
     var body: some View {
         VStack {
-            
-            HStack{
+            HStack {
                 Text("PantryPal")
                     .foregroundStyle(.blue)
                 Spacer()
             }
             .padding(.leading, 15.0)
             
-            HStack{
+            HStack {
                 Text("Ingredientes")
                     .font(.largeTitle)
                     .bold()
@@ -52,8 +52,60 @@ struct IngredientesView: View {
                 AgregarIngredientesView(ingredientes: $ingredientes)
             }
             .padding(.bottom, 10)
+            
+            Button(action: {
+                fetchRecetas() // Llamar a la función para hacer el fetch
+            }) {
+                Text("Buscar Recetas")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 10)
+            }
         }
     }
+    
+    // Función para hacer fetch a la API de Spoonacular
+    private func fetchRecetas() {
+        let ingredientesQuery = ingredientes.joined(separator: ",")
+        let apiKey = "ee0e5dfda0b94ae2a6bf5dbb4a9195af"
+        let urlString = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=\(ingredientesQuery)&apiKey=\(apiKey)&number=5&ranking=2&ignorePantry=true"
+        
+        guard let url = URL(string: urlString) else {
+            print("URL no válida")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error en la solicitud: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No se recibieron datos")
+                return
+            }
+            
+            do {
+                let recetasResponse = try JSONDecoder().decode([Receta].self, from: data)
+                DispatchQueue.main.async {
+                    self.recetas = recetasResponse
+                }
+            } catch {
+                print("Error al decodificar la respuesta: \(error)")
+            }
+        }.resume()
+    }
+}
+
+// Modelo para la respuesta de Spoonacular
+struct Receta: Identifiable, Decodable {
+    let id: Int
+    let title: String
+    let image: String
 }
 
 struct IngredientesView_Previews: PreviewProvider {
